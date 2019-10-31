2f7a7f74f3b842a95d2c1b6db4ece69b6e46e9d5
Ronald Buder <ronald.buder@gmail.com>
2019-10-31 20:53:49 +0800

    Rewritten to Mattermost, clumsily

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 if [ "$VERBOSE" = "true" ]; then
-    python main.py --token $TOKEN --loglevel=$LOGLEVEL --verbose > pybot.log 2> pybot.error.log &
+    python main.py --loglevel=$LOGLEVEL --verbose > pybot.log 2> pybot.error.log &
 else
-    python main.py --token $TOKEN --loglevel=$LOGLEVEL > pybot.log 2> pybot.error.log &
+    python main.py --loglevel=$LOGLEVEL > pybot.log 2> pybot.error.log &
 fi
 
 sed -i -e "s/loki:3100/${LOKIURL}:${LOKIPORT}/g" promtail.yaml
